b751c534e28d178b744545c4645de436513d32ca
adis1313 <adis1313@wp.pl>
2014-08-19 11:04:57 +0200

    Added FB_MSM_LCD_NOTIFY

--- drivers/video/msm/mipi_lgit.c
+++ drivers/video/msm/mipi_lgit.c
@@ -18,6 +18,9 @@
  *
  */
 #include <linux/gpio.h>
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+#include <linux/lcd_notify.h>
+#endif
 
 #include "msm_fb.h"
 #include "mipi_dsi.h"
@@ -66,6 +69,10 @@ static int mipi_lgit_lcd_on(struct platform_device *pdev)
 	struct msm_fb_data_type *mfd;
 	int ret = 0;
 
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+	lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
+#endif
+
 	pr_info("%s started\n", __func__);
 
 	mfd = platform_get_drvdata(pdev);
@@ -113,6 +120,9 @@ static int mipi_lgit_lcd_on(struct platform_device *pdev)
 		return ret;
 	}
 
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+	lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
+#endif
 	pr_info("%s finished\n", __func__);
 	return 0;
 }
@@ -122,6 +132,9 @@ static int mipi_lgit_lcd_off(struct platform_device *pdev)
 	struct msm_fb_data_type *mfd;
 	int ret = 0;
 
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+	lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
+#endif
 	pr_info("%s started\n", __func__);
 
 	if (mipi_lgit_pdata->bl_pwm_disable)
@@ -160,6 +173,10 @@ static int mipi_lgit_lcd_off(struct platform_device *pdev)
 		pr_err("%s: failed to transmit power_off_set_2 cmds\n", __func__);
 		return ret;
 	}
+
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
+#endif
 
 	pr_info("%s finished\n", __func__);
 	return 0;
